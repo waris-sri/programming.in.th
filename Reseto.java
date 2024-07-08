c559ecbcbeb996275ad068bde9c8e6b50cc5a682
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-07-09 00:16:27 +0700

    Added comments in Reseto

--- Reseto.java
+++ Reseto.java
@@ -5,14 +5,13 @@ public class Reseto {
         try (Scanner sc = new Scanner(System.in)) {
             int n = sc.nextInt();
             int k = sc.nextInt();
-            int[] arr = new int[n + 1];
+            int[] arr = new int[n + 1]; // Stores the count of prime numbers
             solve(n, k, arr);
         }
     }
 
     static void solve(int n, int k, int[] arr) {
         int cnt = 0;
-        // Use 2-based index array
         for (int i = 2; i <= n; i++) {
             if (arr[i] == 0) {
                 for (int j = i; j <= n; j += i) {
@@ -28,3 +27,15 @@ public class Reseto {
         }
     }
 }
+
+/*
+ * Use 2-index array since sequence starts with 2 and for straightforward
+ * reference
+ * Initially set all elements in array to 0
+ * Prepare a counter to increment each time when:
+ * - the element at current index is 0
+ * - `i` and `j` are perfectly divisible
+ * - `i` = index, `j` = multiple of `i` (`j += i`)
+ * The counter will increase while marking the processed numbers
+ * When the counter reached `k`, print current index, and halt program
+ */
